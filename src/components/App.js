c430c57aa321f16179557749dace87b02f080fec
Antoni <ant.morawski@gmail.com>
2020-04-11 23:21:11 +0200

    refactored to .env

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from "react"
 import SearchBar from "./SearchBar"
-import { yt, API_KEY } from "../api/yt"
+import { yt } from "../api/yt"
 
 class App extends React.Component {
   state = { vids: [] }
 
   onSearchSubmit = async (term) => {
     const response = await yt.get("/search", {
-      params: { q: term, part: "snippet", type: "video", key: API_KEY },
+      params: { q: term, part: "snippet", type: "video", key: process.env.REACT_APP_API_KEY },
     })
     this.setState({ vids: response.data.items })
     console.log(this.state.vids)
